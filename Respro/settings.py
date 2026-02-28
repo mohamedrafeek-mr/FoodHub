35ea46faec5770ea2f6fda058ec7e6fd7da7819c
Mohamed Rafeek M <rafirafibai@gmail.com>
2026-02-28 14:22:49 +0530

    Update settings.py

--- Respro/settings.py
+++ Respro/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bsw4p!zvwo%%w$421+ved9g^g7sqry80@ac0@!_(cccoyagp*('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
